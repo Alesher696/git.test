a3667d890bc96d072452a177888e179528f411ee
Alexandr Sherstnev <alesher696@gmail.com>
2022-11-28 20:56:26 +0300

    +2 test

--- index2.js
+++ index2.js
@@ -1,3 +1,30 @@
-console.log(alert("test"))
+// let age = prompt("Сколько Вам лет?", 18);
 
-let newArray = [];
+// let welcome = (age < 18) ?
+//   function() { alert("Привет!"); } :
+//   function() { alert("Здравствуйте!"); };
+
+// welcome();
+
+// let func = (num) => num.toString();
+
+// console.log(func(21))
+// console.log(typeof(func(21)))
+
+
+
+
+// let total; 
+// function makeNegative(num) {
+    
+//             total = (num < 0) ? num : 
+//                 (num == 0) ? num * 0 :
+//                 (num > 0) ? num * -1 : num;
+//    return total;
+//   }
+//   console.log(makeNegative(0))
+
+
+
+
+

2ee3e47b9a1023861201199cb4088db9c3ec9990
Alexandr Sherstnev <alesher696@gmail.com>
2022-11-30 13:53:01 +0300

    + codewars

---
+++ index.js
@@ -0,0 +1,64 @@
+
+// let message;
+// let func = (login)=>
+// (login == 'Сотрудник') ?
+//   message = 'Привет' : (login == 'Директор') ? 
+//   message = 'Здравствуйте' : (login == '') ?
+//   message = 'Нет логина' : message = '';
+
+// console.log(func("Директор"))
+
+
+
+
+//  const myArr = [2, 3, 4, 5, 6];
+// let total = 0;
+
+// for (let i = 0; i < myArr.length; i+=1){
+//   total += myArr[i]
+// }
+
+// console.log(total)
+
+// let arr = [1,2,3,4,5,6,7]
+
+
+// const deleteWords = arr.filter((element, index, arr)=>{
+//   if(index % 2 == 0){
+//     return true;
+//   }
+// })
+// console.log(deleteWords)
+
+
+// function removeEveryOther(arr){
+  
+//   const deleteWords = arr.filter((element, index, arr)=>{
+//   if(index % 2 == 0){
+//     return true;
+//   }
+// })
+//   return deleteWords;
+// }
+ 
+
+
+// function check(a, x) {
+
+//   for( let i = 0; i < a.length; i++){
+//     if (a[i] === x){
+//       return true
+//     }else return false
+    
+//     }
+//   }
+  
+
+//   console.log(check([66,101],101))
+
+
+function updateLight(current) {
+  
+    //your code here!
+  
+  }

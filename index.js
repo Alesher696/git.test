93c6c41661db07fee080136a7dc3dca5bc1c66f0
Alexandr Sherstnev <alesher696@gmail.com>
2022-12-01 11:25:47 +0300

    +3

--- index.js
+++ index.js
@@ -57,46 +57,90 @@
 //   console.log(check([66,101],101))
 
 
-function updateLight(current) {
-  let arr = ["green", "red", "yellow"];
- return (current == 'green') ? "yellow" : (current == 'yellow') ? "red" : (current == 'red') ? "green" : "yellow"
+// function updateLight(current) {
+//   let arr = ["green", "red", "yellow"];
+//  return (current == 'green') ? "yellow" : (current == 'yellow') ? "red" : (current == 'red') ? "green" : "yellow"
   
-  }
+//   }
 
-  console.log(updateLight('green'))
-
-
+//   console.log(updateLight('green'))
 
 
-//   100000/ 3600
-function cockroachSpeed(s) {
+
+
+// //   100000/ 3600
+// function cockroachSpeed(s) {
   
-let cm = parseInt((s * 100000) / 3600)
-return cm;
+// let cm = parseInt((s * 100000) / 3600)
+// return cm;
 
-  }
+//   }
 
-  console.log(cockroachSpeed(1.08))
-
-
+//   console.log(cockroachSpeed(1.08))
 
 
-  function rowSumOddNumbers(n) {
-    let arr = Array(n).fill().map((el, i) => i+1);
-    let sum = 0;
-    if( n == 0){return 0}
-    else{for (let i= 0; i < arr.length; i++){
-        sum += i;
+
+
+//   function rowSumOddNumbers(n) {
+//     let arr = Array(n).fill().map((el, i) => i+1);
+//     let sum = 0;
+//     if( n == 0){return 0}
+//     else{for (let i= 0; i < arr.length; i++){
+//         sum += i;
+//     }
+//     return sum ;}
+    
+    
+    
+
+// }
+
+
+// console.log(rowSumOddNumbers(42))
+
+
+
+// function sum (numbers) {
+//     let arr = 0;
+//     if( numbers.length <= 0){
+//         return 0;
+//     } else if ( numbers.length > 0 ){
+//     for ( let i = 0; i < numbers.length; i++){
+        
+//         arr += numbers[i];
+        
+
+       
+//     }
+//     return arr
+//     }
+    
+    
+// };
+
+
+// let newArr = [1,2,3,4]
+// console.log(sum(newArr))
+
+
+
+
+  
+  function descendingOrder(n){
+
+    let newarr = (('' + n).split('')).map(Number)
+    newarr.sort(function(a, b) {
+       return b - a;})
+
+    let arr2 = parseInt(newarr.join(''))
+    
+ 
+    return arr2
+
     }
-    return sum ;}
-    
-    
-    
-
-}
+   
+ 
 
 
-console.log(rowSumOddNumbers(42))
-
-
-
+  console.log(descendingOrder(123456))
+  console.log(typeof(descendingOrder(12345)))

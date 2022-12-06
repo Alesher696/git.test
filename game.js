dc06c9a1633ca1a2e0401fa414b1285bf31f43d0
Alexandr Sherstnev <alesher696@gmail.com>
2022-12-06 15:08:09 +0300

    ++

---
+++ game.js
@@ -0,0 +1,20 @@
+"Use strict"
+let secretNumber = Math.floor(Math.random()*20) + 1;
+
+document.querySelector('.letsTry').textContent;
+
+console.log(document.querySelector('.letsTry').textContent);
+
+
+
+// function changeInfo(){
+// // let a = document.querySelector('.letsTry').textContent
+// // let b = document.querySelector('.score').textContent
+// // let c = document.querySelector('.trying').textContent
+// // let d = document.querySelector('windowEnter').value;
+// // if(d > secretNumber){
+// //     return document.querySelector('.letsTry').textContent = "More!"
+// // } else return document.querySelector('.letsTry').textContent = "Less!"
+// console.log(windowEnter.value);
+// }
+
